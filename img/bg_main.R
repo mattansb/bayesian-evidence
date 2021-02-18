756036c9bf38dc826ddc9cd2bcc6b2e0521ef740
mattansb <35330040+mattansb@users.noreply.github.com>
2021-02-18 14:40:55 +0200

    redesign slides

--- img/bg_main.R
+++ img/bg_main.R
@@ -9,7 +9,7 @@ L <-
     aes(color = .x),
     size = 1,
     # alpha = 1 - .x/2,
-    alpha = 0.8,
+    alpha = 0.3,
     fun = dnorm,
     xlim = c(-3, 5),
     args = list(mean = .x*1.5, sd = .x + 1)
@@ -19,7 +19,7 @@ ggplot() +
   L +
   scale_color_material_c() +
   theme_minimal() +
-  theme(plot.background = element_rect(fill = "#1381B0"),
+  theme(plot.background = element_rect(fill = "#182933"),
         axis.text = element_blank(),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
